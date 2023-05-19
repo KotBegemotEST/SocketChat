0fc9d33e7485914744ff712995014607942ab955
Anton Buketov <38258424+KotBegemotEST@users.noreply.github.com>
2023-05-19 21:04:31 +0300

    ready Client

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -3,37 +3,64 @@ import queryString from 'query-string';
 import io from "socket.io-client";
 import { BrowserRouter as Router, Switch, Route, useLocation } from "react-router-dom";
 
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
 let socket;
 
 const Chat = () => {
   const [name,setName] = useState('')
   const [room,setRoom] = useState('')
+  const [message, setMessage] = useState('');
+  const [users, setUsers] = useState('');
+  const [messages, setMessages] = useState([]);
   const location = useLocation();
   const ENDPOINT = 'localhost:5000'
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
 
- useEffect(()=>{
-
-    const {name,room} = queryString.parse(location.search);
     socket = io(ENDPOINT);
-    setName(name)
-    setRoom(room)
 
-    console.log(socket)
-    socket.emit('join',{name,room},()=>{
-      // alert(error)
-    })
-    return ()=>{
-      socket.emit("disconnect")
-      socket.off();
+    setRoom(room);
+    setName(name)
+
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
     }
- },[ENDPOINT,location.search]);
+  }
 
   return (
-    <>
-      <h1>Chat</h1>
-    </>
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users}/>
+    </div>
   );
-
 }
 
 export default Chat;
